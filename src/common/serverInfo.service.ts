b10bbbdd44894622892fe9c79112c283b8303a6d
Tang <11726782+sugar_gitee@user.noreply.gitee.com>
2023-11-28 17:01:38 +0800

    feat: 获取项目的ip和启动端口

---
+++ src/common/serverInfo.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import * as os from 'os';
+
+@Injectable()
+export class ServerInfoService {
+  getLocalIP(): string {
+    const ifaces = os.networkInterfaces();
+    let localIP = '';
+
+    Object.keys(ifaces).forEach((ifname) => {
+      ifaces[ifname].forEach((iface) => {
+        if (iface.family === 'IPv4' && !iface.internal) {
+          localIP = iface.address;
+        }
+      });
+    });
+    return localIP + ':' + process.env.PORT;
+  }
+}
