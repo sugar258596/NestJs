e6cda55f81ee3931e0e5dea740019eee0768ea0c
Tang <11726782+sugar_gitee@user.noreply.gitee.com>
2023-11-30 10:48:07 +0800

    ci: 开启登录限制

--- src/auth/middleware/login-throttler.middleware.ts
+++ src/auth/middleware/login-throttler.middleware.ts
@@ -4,16 +4,16 @@ import {
   Injectable,
   NestMiddleware,
 } from '@nestjs/common';
-import type { Request, Response, NextFunction } from 'express';
+import type { NextFunction } from 'express';
 
 @Injectable()
 export class LoginThrottlerMiddleware implements NestMiddleware {
   private count = 0;
   private blocked = false;
-  private Max = 1000;
+  private Max = 5;
   private time = 2;
 
-  use(req: Request, res: Response, next: NextFunction) {
+  use(_, __, next: NextFunction) {
     if (this.blocked)
       throw new HttpException('稍后再试!!', HttpStatus.TOO_MANY_REQUESTS);
     this.count++;
