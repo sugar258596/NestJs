501136d60f0184d6c6ee41a13fb311a03a8729cc
Tang <11726782+sugar_gitee@user.noreply.gitee.com>
2024-06-17 15:16:37 +0800

    ci: 设置环境变量

--- src/mysql/mysl.config.ts
+++ src/mysql/mysl.config.ts
@@ -9,8 +9,8 @@ export const getDatabaseConfigAsync = async (
     username: configService.get('DB_USERNAME'), //账号
     password: configService.get('DB_PASSWROD'), //密码
     host: configService.get('DB_URL'), //host
-    port: 3306, // 端口号
-    database: 'nest', //库名
+    port: configService.get('DB_PORT'), // 端口号
+    database: configService.get('DB_DATABASE'), //库名
     // entities: [__dirname + '/**/*.entity{.ts,.js}'], //实体文件
     synchronize: true, //synchronize字段代表是否自动将实体类同步到数据库
     retryDelay: 500, //重试连接数据库间隔
